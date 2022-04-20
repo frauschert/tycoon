5305b899dd0e16ca93cd3b2f52f8332b1d791651
Fabian Rauschert <fabian.doell@freenet.de>
2022-04-20 22:28:34 +0200

    'init'

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+function render() {
+  ReactDOM.render(
+    <>
+      <h1>Hello from React!</h1>
+      <h2>Hansi muffelt!!!</h2>
+    </>,
+    document.body
+  );
+}
+
+render();
