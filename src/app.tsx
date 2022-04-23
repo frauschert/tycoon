bf71a6ad903ca4706f3edccd445198ba9cf1555a
frauschert <fabian.rauschert@freenet.de>
2022-04-23 21:33:27 +0200

    use createRoot of React 18

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,10 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
-function render() {
-  ReactDOM.render(
-    <>
-      <h1>Hello from React!</h1>
-    </>,
-    document.body
-  );
-}
-
-render();
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <>
+    <h1>Hello from React!</h1>
+  </>
+);
