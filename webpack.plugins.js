71b220b79bc81a601883094b339b5e713eb4a945
frauschert <fabian.rauschert@freenet.de>
2022-04-23 21:24:39 +0200

    added debug possibility

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,3 +1,10 @@
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+/* disable ForkTsCheckerWebpackPlugin to enable debugging. 
+Bug is tracked here: https://github.com/electron-userland/electron-forge/issues/2681 
+https://stackoverflow.com/questions/71673968/white-window-when-debugging-electron-forge-typescript-webpack-app-from-vscod 
+*/
 
-module.exports = [new ForkTsCheckerWebpackPlugin()];
+//const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = [
+  /*new ForkTsCheckerWebpackPlugin()*/
+];
